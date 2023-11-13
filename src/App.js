17c3e0a4708bfac19db3fa8897760e8dfe8add5b
Hudah Nur <hudahn424@gmail.com>
2023-11-13 13:16:15 -0600

    make image closer to title

--- src/App.js
+++ src/App.js
@@ -16,7 +16,10 @@ function App() {
     <div className="App">
   <header className="App-header">
   <div id="img-div" class="image-housing">
-    <img id="image" class="center" src="https://drive.google.com/uc?export=view&id=1bDdoLrKDvqQ8q3-mUG8ke1UQgH88D0FO"
+    <h1>Game Name</h1>
+    <img id="image" class="center" 
+    src="https://drive.google.com/uc?
+    export=view&id=1bDdoLrKDvqQ8q3-mUG8ke1UQgH88D0FO"
      alt="Dungeon room 1"></img>
   </div>
   </header>
