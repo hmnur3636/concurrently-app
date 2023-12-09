309a2fccb6d9ebff74dcac2a9378c8ad53fcd97b
WMonin <148662778+WMonin@users.noreply.github.com>
2023-12-09 10:45:42 -0600

    Built Decision System

--- src/App.js
+++ src/App.js
@@ -1,55 +1,36 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import './App.css';
+import { scenarios } from './Script';
 
 function App() {
-  const [users, setUsers] = useState ([]);
+  const [currentScenario, setCurrentScenario] = useState(scenarios[0]);
 
-  useEffect(() => {
-    fetch('/api/users')
-    .then((res) => res.json())
-    .then ((data) => setUsers(data));
-}, []);
+  const handleChoice = (nextScenarioId) => {
+    const nextScenario = scenarios.find(scenario => scenario.id === nextScenarioId);
+    setCurrentScenario(nextScenario);
+  };
 
   return (
-    
     <div className="App">
-    <div className ="head"><h2>Developing Dungeon</h2></div>
-  <main className="App-header">
+      <div className='App-header'>
+      <h2>Developing Dungeon</h2>
+        <div className='Background-Screen'>
+      <img src={currentScenario.image} alt="Current Scenario" />
+      </div>
+      <div className='container'>
+        <p>{currentScenario.text}</p>
+      <div className="btn-grid">
+        {currentScenario.choices.map(choice => (
+          <button className="btn" key={choice.nextId} onClick={() => handleChoice(choice.nextId)}>
+            {choice.text}
+          </button>
+        ))}
+      </div>
+      </div>
 
-    <div className="Background-Screen">
-  <div id="img-div" class="image-housing">
-    <img id="image" class="center" 
-    src="https://drive.google.com/uc?
-    export=view&id=1bDdoLrKDvqQ8q3-mUG8ke1UQgH88D0FO"
-     alt="Dungeon room 1"></img>
-  </div>
-  </div>
-  <div class="container">
-        <div id="text">
-          <p>You wake up at the gates of a dungeon. The last thing you remember is that 
-            you were sailing when suddenly a dangerous storm occurred. 
-            The storm tore apart your ship and everything went black. 
-            Looking around the dungeon entrance you see a note left on the floor in front of you. 
-            You see some chains, bones, and weapons on the walls. 
-            There is also a door that is open and light floods out from it. 
-            Going through the door seems to be your 
-            only way forward as going out leads to the beach you arrived on. 
-            What do you want to do? 
-          </p>
-          </div>
-        <div id="button-options" class="btn-grid">
-        <button type="button" class="btn btn-option1">Option 1</button>
-        <button type="button" class="btn btn-option2">Option 2</button>
-        <button type="button" class="btn btn-option3">Option 3</button>
-        <button type="button" class="btn btn-option4">Option 4</button>
-        </div>
-        </div>
-  </main>
-  </div>
-    )
-    
-};
-
+      </div>
+    </div>
+  );
+}
 
 export default App;
---
+++ src/Script.js
@@ -0,0 +1,85 @@
+export const scenarios = [
+    {
+      id: 0,
+      text: 'You wake up at the gates of a dungeon. The last thing you remember is that you were sailing when suddenly a dangerous storm occurred. The storm tore apart your ship and everything went black. Looking around the dungeon entrance you see a note left on the floor in front of you. You see some chains, bones, and weapons on the walls. There is also a door that is open and light floods out from it. Going through the door seems to be your only way forward as going out leads to the beach you arrived on. What do you want to do?',
+      image: 'https://drive.google.com/uc?export=view&id=1bDdoLrKDvqQ8q3-mUG8ke1UQgH88D0FO', 
+      choices: [
+        { text: 'Read Note', nextId: 1 },
+        { text: 'Investigate the Chains', nextId: 2 },
+        { text: 'Investigate the Bones', nextId: 3 },
+        { text: 'Leave The Room', nextId: 4 }
+      ]
+    },
+
+    {
+      id: 1,
+      text: 'You pick up the note off of the floor. Despite it seeming to have been in the dungeon for a while the paper is still crisp and fresh and ink still glistens in the light. Something feels off not just about the note being new, but also the dungeon itself. You feel a sense of dread but you decide to see what the note has to say anyways. The note reads “If it is freedom you seek then heed well my warnings. The left is filled with peril and it will fail you. Follow the right in the light and you will not fail. Stay on the right with the light and do not follow the darkness to the left.”',
+      image: 'https://drive.google.com/uc?export=view&id=1bDdoLrKDvqQ8q3-mUG8ke1UQgH88D0FO', 
+      choices: [
+        { text: 'Read Note Again', nextId: 1 },
+        { text: 'Investigate the Chains', nextId: 2 },
+        { text: 'Investigate the Bones', nextId: 3 },
+        { text: 'Leave The Room', nextId: 4 }
+  ]
+ },
+
+ {
+  id: 2,
+  text: 'You pick up the note off of the floor. Despite it seeming to have been in the dungeon for a while the paper is still crisp and fresh and ink still glistens in the light. Something feels off not just about the note being new, but also the dungeon itself. You feel a sense of dread but you decide to see what the note has to say anyways. The note reads “If it is freedom you seek then heed well my warnings. The left is filled with peril and it will fail you. Follow the right in the light and you will not fail. Stay on the right with the light and do not follow the darkness to the left.”',
+  image: 'https://drive.google.com/uc?export=view&id=1bDdoLrKDvqQ8q3-mUG8ke1UQgH88D0FO', 
+  choices: [
+    { text: 'Read Note Again', nextId: 1 },
+    { text: 'Investigate the Chains', nextId: 2 },
+    { text: 'Investigate the Bones', nextId: 3 },
+    { text: 'Leave The Room', nextId: 4 }
+]
+},
+
+{
+  id: 3,
+  text: 'You pick up the note off of the floor. Despite it seeming to have been in the dungeon for a while the paper is still crisp and fresh and ink still glistens in the light. Something feels off not just about the note being new, but also the dungeon itself. You feel a sense of dread but you decide to see what the note has to say anyways. The note reads “If it is freedom you seek then heed well my warnings. The left is filled with peril and it will fail you. Follow the right in the light and you will not fail. Stay on the right with the light and do not follow the darkness to the left.”',
+  image: 'https://drive.google.com/uc?export=view&id=1bDdoLrKDvqQ8q3-mUG8ke1UQgH88D0FO', 
+  choices: [
+    { text: 'Read Note Again', nextId: 1 },
+    { text: 'Investigate the Chains', nextId: 2 },
+    { text: 'Investigate the Bones', nextId: 3 },
+    { text: 'Leave The Room', nextId: 4 }
+]
+},
+
+{
+  id: 4,
+  text: 'You duck under the portcullis and the points of it poke you wanting to hurt you. For a second you think the dungeon might be trying to kill you, but that is silly magic does not exist and neither do dungeons that are alive. You walk out on the other side, your eyes adjusting in the light as the brightness overwhelms you. It feels smothering and you want to flee, but you get used to it as your eyes adjust. To your right a stairway leads up while a stairway going down is on the left. Which path will you choose?',
+  image: 'https://drive.google.com/uc?export=view&id=1qUZv3VSWv0SzrgTJtDGCoDo7BnVYG4qL', 
+  choices: [
+    { text: 'Go left', nextId: 6 },
+    { text: 'Go Right', nextId: 7 },
+    { text: 'Let me read This Note Real Quick', nextId: 5 },
+    { text: 'Never Mind, I Will Go Back', nextId: 1 },
+]
+},
+
+{
+  id: 5,
+  text: 'You duck under the portcullis and the points of it poke you wanting to hurt you. For a second you think the dungeon might be trying to kill you, but that is silly magic does not exist and neither do dungeons that are alive. You walk out on the other side, your eyes adjusting in the light as the brightness overwhelms you. It feels smothering and you want to flee, but you get used to it as your eyes adjust. To your right a stairway leads up while a stairway going down is on the left. Which path will you choose?',
+  image: 'https://drive.google.com/uc?export=view&id=1qUZv3VSWv0SzrgTJtDGCoDo7BnVYG4qL', 
+  choices: [
+    { text: 'Go left', nextId: 6 },
+    { text: 'Go Right', nextId: 7 },
+    { text: 'Let me read This Note Real Quick', nextId: 5 },
+    { text: 'Never Mind, I Will Go Back', nextId: 1 },
+]
+},
+
+{
+  id: 7,
+  text: 'You duck under the portcullis and the points of it poke you wanting to hurt you. For a second you think the dungeon might be trying to kill you, but that is silly magic does not exist and neither do dungeons that are alive. You walk out on the other side, your eyes adjusting in the light as the brightness overwhelms you. It feels smothering and you want to flee, but you get used to it as your eyes adjust. To your right a stairway leads up while a stairway going down is on the left. Which path will you choose?',
+  image: 'https://drive.google.com/uc?export=view&id=1flTub35yMfrU7nFv20LZOsp17tFubXgd', 
+  choices: [
+    { text: 'Go left', nextId: 5 },
+    { text: 'Go Right', nextId: 6 },
+    { text: 'Never Mind, I Will Go Back', nextId: 1 },
+]
+},
+  ];
+
